33eb4d88ecae7095a790f2f1cb12c0e1fb837e71
dawid.krawczyk <dawid.krawczyk@kernelpanic.com.pl>
2021-01-05 16:27:35 +0100

    Add entity of warehouse features

---
+++ warehouse/src/main/java/com/example/warehouse/model/Material.java
@@ -0,0 +1,125 @@
+package com.example.warehouse.model;
+
+import org.hibernate.envers.Audited;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Audited
+public class Material {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long nofMaterial;
+    private double width;
+    private double height;
+    private double length;
+    private double weight;
+    private LocalDate dateOfUse;
+    private int priority;
+    private String supplier;
+    private String supplierCountry;
+    private String status;
+
+    @OneToOne
+    private RackSpace rackSpace;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNofMaterial() {
+        return nofMaterial;
+    }
+
+    public void setNofMaterial(Long nofMaterial) {
+        this.nofMaterial = nofMaterial;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public LocalDate getDateOfUse() {
+        return dateOfUse;
+    }
+
+    public void setDateOfUse(LocalDate dateOfUse) {
+        this.dateOfUse = dateOfUse;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getSupplierCountry() {
+        return supplierCountry;
+    }
+
+    public void setSupplierCountry(String supplierCountry) {
+        this.supplierCountry = supplierCountry;
+    }
+
+    public RackSpace getRackSpace() {
+        return rackSpace;
+    }
+
+    public void setRackSpace(RackSpace rackSpace) {
+        this.rackSpace = rackSpace;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ warehouse/src/main/java/com/example/warehouse/model/RackSpace.java
@@ -0,0 +1,90 @@
+package com.example.warehouse.model;
+
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Audited
+public class RackSpace {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long rackId;
+
+    private String status;
+    private double width;
+    private double height;
+    private double length;
+    private double maxWeight;
+    private int priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRackId() {
+        return rackId;
+    }
+
+    public void setRackId(Long rackId) {
+        this.rackId = rackId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(double maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
